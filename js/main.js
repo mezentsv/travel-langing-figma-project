6ad36a9f227db3c7f39f4034ff154e6669513534
Artem Mezentsev <mezentsv@gmail.com>
2020-10-07 01:19:29 +0300

    Make the header adaptive: add menu with nav-links for max-width: 767px

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@
     }
   };
 })();
+
+// Burger handler
+(function () {
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header-nav");
+  const menuCloseItem = document.querySelector(".header-nav-close");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header-nav-active");
+  });
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("header-nav-active");
+  });
+})();
