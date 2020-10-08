0c755147fdf3c516938640a4f2aa7f31b9d76dc6
Artem Mezentsev <mezentsv@gmail.com>
2020-10-08 22:42:19 +0300

    Add script for hiding menu when clicking on links (Mobile). Correct semantic mistakes

--- js/main.js
+++ js/main.js
@@ -14,12 +14,20 @@
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header-nav");
   const menuCloseItem = document.querySelector(".header-nav-close");
+  const menuLinks = document.querySelectorAll(".header-link");
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header-nav-active");
   });
   menuCloseItem.addEventListener("click", () => {
     menu.classList.remove("header-nav-active");
   });
+  if (window.innerWidth < 768) {
+    for (let i = 0; i < menuLinks.length; i++) {
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header-nav-active");
+      });
+    }
+  }
 })();
 // Scroll to anchors
 (function () {
